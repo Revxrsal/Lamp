f2e94f05cbfd27cce7bb7fcbd6e89658a1bc6a39
Revxrsal <aloosh.kasasbeh@gmail.com>
2021-09-17 23:44:14 +0300

    Delete Test.java

--- common/src/main/java/revxrsal/commands/util/tokenize/Test.java
+++
@@ -1,9 +0,0 @@
-package revxrsal.commands.util.tokenize;
-
-public class Test {
-
-    public static void main(String[] args) {
-        System.out.println(QuotedStringTokenizer.tokenize("hello gamers \"omg super"));
-    }
-
-}
