13c7daee3049999ebf525c64d4123fc233618f35
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2021-10-06 13:44:44 +0300

    Do not join string arrays

--- common/src/main/java/revxrsal/commands/command/ArgumentStack.java
+++ common/src/main/java/revxrsal/commands/command/ArgumentStack.java
@@ -91,7 +91,7 @@ public interface ArgumentStack extends Deque<String>, List<String>, Cloneable {
      */
     static @NotNull ArgumentStack of(@NotNull String... arguments) {
         if (arguments.length == 0) return empty();
-        return new LinkedArgumentStack(String.join(" ", arguments));
+        return new LinkedArgumentStack(arguments);
     }
 
     /**
