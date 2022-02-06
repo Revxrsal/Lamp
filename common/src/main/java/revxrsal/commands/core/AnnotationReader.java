721a1f1e943bc9fb8651d5d9546b66a0b6236ecd
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2022-02-06 13:34:10 +0200

    Implement the new orphan commands API

--- common/src/main/java/revxrsal/commands/core/AnnotationReader.java
+++ common/src/main/java/revxrsal/commands/core/AnnotationReader.java
@@ -1,11 +1,16 @@
 package revxrsal.commands.core;
 
 import org.jetbrains.annotations.NotNull;
-import revxrsal.commands.annotation.*;
+import revxrsal.commands.annotation.Command;
+import revxrsal.commands.annotation.Default;
+import revxrsal.commands.annotation.DistributeOnMethods;
+import revxrsal.commands.annotation.Subcommand;
+import revxrsal.commands.orphan.OrphanCommand;
+import revxrsal.commands.orphan.OrphanRegistry;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.*;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -21,25 +26,43 @@ public final class AnnotationReader implements Iterable<Annotation> {
     private final Map<Class<?>, Annotation> annotations = new HashMap<>();
     private boolean empty = false;
 
-    public AnnotationReader(AnnotatedElement element) {
-        for (Annotation annotation : element.getDeclaredAnnotations()) {
+    public AnnotationReader(Parameter parameter) {
+        for (Annotation annotation : parameter.getDeclaredAnnotations()) {
             annotations.put(annotation.annotationType(), annotation);
         }
     }
 
-    public AnnotationReader(Class<?> container, Method element) {
+    private static Command createCommand(List<CommandPath> paths) {
+        String[] pathsArray = paths.stream().map(CommandPath::toRealString).toArray(String[]::new);
+        return new Command() {
+            @Override public Class<? extends Annotation> annotationType() {return Command.class;}
+
+            @Override public String[] value() {return pathsArray;}
+        };
+    }
+
+    public AnnotationReader(Class<?> container, Method element, @NotNull Object target) {
         Annotation[] method = element.getDeclaredAnnotations();
         boolean ignore = true;
+
+        // methods that have none of our command annotations should
+        // be discarded and ignored
         for (Annotation annotation : method) {
             if (ANNOTATIONS.contains(annotation.annotationType())) {
                 ignore = false;
                 break;
             }
         }
+
         if (ignore) {
             empty = true;
             return;
         }
+
+        if (OrphanCommand.class.isAssignableFrom(container)) { // do it first since it's the lowest priority.
+            annotations.put(Command.class, createCommand(((OrphanRegistry) target).getParentPaths()));
+        }
+
         for (Annotation annotation : element.getDeclaringClass().getDeclaredAnnotations()) {
             if (annotation.annotationType().isAnnotationPresent(DistributeOnMethods.class)) {
                 annotations.put(annotation.annotationType(), annotation);
