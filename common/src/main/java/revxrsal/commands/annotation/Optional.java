1e9e954c039347ff9f04d45c1792175dc87cc90f
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-03-02 22:14:12 +0300

    Update docs of @Optional

--- common/src/main/java/revxrsal/commands/annotation/Optional.java
+++ common/src/main/java/revxrsal/commands/annotation/Optional.java
@@ -45,6 +45,8 @@ public @interface Optional {
     /**
      * The default value of the parameter. When not specified, the parameter
      * will be marked as null.
+     * <p>
+     * Note that this may be compacted using {@link Default @Default("...")}
      *
      * @return The default value of the parameter
      */
