1eaef4ac92e356c4f197cc18baa9d332ba26c3e0
bivashy <botyrbojey@gmail.com>
2023-06-22 00:29:32 +0600

    Add documentation for SlashCommandAdapter, SlashCommandMapper

--- jda/src/main/java/revxrsal/commands/jda/SlashCommandMapper.java
+++ jda/src/main/java/revxrsal/commands/jda/SlashCommandMapper.java
@@ -6,8 +6,15 @@ import revxrsal.commands.command.ExecutableCommand;
 import revxrsal.commands.jda.core.adapter.SlashCommandAdapter;
 
 /**
- * Maps {@link ExecutableCommand} into {@link }
+ * Modifies {@link SlashCommandAdapter} based on {@link ExecutableCommand}.
  */
 public interface SlashCommandMapper {
+    /**
+     *  Modifies existing slash command, or subcommand. For example changing description of slash commands.
+     *
+     * @param slashCommandAdapter Slash command or subcommand that will be modified
+     * @param command             Command that 'represents' slash command
+     * @see SlashCommandAdapter
+     */
     void mapSlashCommand(@NotNull SlashCommandAdapter slashCommandAdapter, @NotNull ExecutableCommand command);
 }
