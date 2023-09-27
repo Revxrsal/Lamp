2b1d4c8a2092f7e01034f14fd95189d47534586a
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-09-27 13:51:30 +0300

    replace unicode with actual character

--- common/src/main/java/revxrsal/commands/util/Strings.java
+++ common/src/main/java/revxrsal/commands/util/Strings.java
@@ -136,7 +136,7 @@ public final class Strings {
         char[] b = text.toCharArray();
         for (int i = 0; i < b.length - 1; i++) {
             if (b[i] == '&' && "0123456789AaBbCcDdEeFfKkLlMmNnOoRrXx".indexOf(b[i + 1]) > -1) {
-                b[i] = '\u00A7';
+                b[i] = '§';
                 b[i + 1] = Character.toLowerCase(b[i + 1]);
             }
         }
