44574eca4cf71786546fde592f23a0069e59237e
Revxrsal <aloosh.kasasbeh@gmail.com>
2024-08-15 23:50:15 +0300

    add junit because we need tests

---
+++ common/build.gradle.kts
@@ -0,0 +1,10 @@
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
