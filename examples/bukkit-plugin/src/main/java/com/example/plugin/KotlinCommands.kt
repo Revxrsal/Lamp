8ce2a2142a9417807fe3b1397b40828b27b3ec50
Revxrsal <aloosh.kasasbeh@gmail.com>
2024-08-25 01:54:52 +0300

    add KotlinCommands

---
+++ examples/bukkit-plugin/src/main/java/com/example/plugin/KotlinCommands.kt
@@ -0,0 +1,20 @@
+package com.example.plugin
+
+import revxrsal.commands.annotation.Command
+import revxrsal.commands.annotation.Optional
+import revxrsal.commands.bukkit.actor.BukkitCommandActor
+
+class KotlinCommands {
+
+    @Command("teleport")
+    fun teleport(
+        actor: BukkitCommandActor,
+        x: Int,
+        y: Int,
+       @Optional z: Int = 10
+    ) {
+        actor.reply("X: $x")
+        actor.reply("Y: $y")
+        actor.reply("Z: $z")
+    }
+}
