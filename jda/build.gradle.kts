0948be4932378cee3391d66ba81f02b3756d81b1
Revxrsal <aloosh.kasasbeh@gmail.com>
2024-08-25 01:55:14 +0300

    remove -parameters from JDA

--- jda/build.gradle.kts
+++ jda/build.gradle.kts
@@ -10,8 +10,3 @@ dependencies {
     implementation(project(":common"))
     compileOnly("net.dv8tion:JDA:5.1.0")
 }
-
-tasks.withType<JavaCompile> {
-    // Preserve parameter names in the bytecode
-    options.compilerArgs.add("-parameters")
-}
