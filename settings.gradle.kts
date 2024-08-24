fd01c22884986bac9046829375cc1a3730a0bce5
Revxrsal <aloosh.kasasbeh@gmail.com>
2024-08-25 01:15:59 +0300

    JDA :tada: aaaaaand we're done!

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
+pluginManagement {
+    plugins {
+        kotlin("jvm") version "2.0.10"
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
@@ -12,3 +18,4 @@ include("brigadier")
 include("sample-plugin")
 include("velocity")
 include("sponge")
+include("jda")
