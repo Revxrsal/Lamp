d333fa5b408009bea51080e3e2d7413c22dde48f
Revxrsal <aloosh.kasasbeh@gmail.com>
2024-08-27 23:53:34 +0300

    start a basic plugin

--- examples/bukkit-plugin/src/main/java/com/example/plugin/TestPlugin.java
+++ examples/bukkit-plugin/src/main/java/com/example/plugin/TestPlugin.java
@@ -1,15 +1,13 @@
 package com.example.plugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import revxrsal.commands.Lamp;
 import revxrsal.commands.bukkit.BukkitLamp;
-import revxrsal.commands.bukkit.actor.BukkitCommandActor;
 
-public class TestPlugin extends JavaPlugin {
+public final class TestPlugin extends JavaPlugin {
 
-    @Override public void onEnable() {
-        Lamp<BukkitCommandActor> lamp = BukkitLamp.builder(this).build();
-        lamp.register(new TeleportCommands());
-        lamp.register(new KotlinCommands());
+    @Override
+    public void onEnable() {
+        var lamp = BukkitLamp.builder(this).build();
+        lamp.register(new GreetCommands());
     }
 }
